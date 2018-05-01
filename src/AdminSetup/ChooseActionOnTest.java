8beb9488e618cd69f51fafcc5cfb6fe42566c14f
githubuser476 <rebecca.kennedy2@pcc.edu>
2018-05-01 10:10:02 -0700

    Added a JList within a JScrollPane to hold the test that the user can select. During Sprint 2, a list of available tests will display here but during Sprint 1, it will just be the one test.

--- src/AdminSetup/ChooseActionOnTest.java
+++ src/AdminSetup/ChooseActionOnTest.java
@@ -11,11 +11,12 @@ import java.awt.event.ActionListener;
  * Description: This GUI allows the admin to choose whether he wants to add another test
  * or edit an existing test. When he makes his choice, the appropriate GUI should display.
  *
- * Currently only two of my GUIs show. I plan ViewTest GUI later. After the Admin selects
- * "Edit/View Test", he will be presented in another GUI a list of the tests in the database.
- * Next to each test, it will display to him whether or not it can be edited or viewed only.
- * After he chooses his test, either the EditTest GUI will display or the ViewTest GUI will
- * display.
+ * Currently only two of my GUIs show. I plan to display ViewTest GUI later.
+ *
+ * After the Admin selects "Edit/View Test", he will be presented within this GUI the test
+ * (When Sprint 2, a list of the tests) in the database. Next to the displayed test(s), it
+ * will display to him whether or not it can be edited or viewed only. After he chooses his
+ * test, either the EditTest GUI will display or the ViewTest GUI will display.
  */
 public class ChooseActionOnTest {
     private static JFrame frame = null;
@@ -24,12 +25,16 @@ public class ChooseActionOnTest {
     private JRadioButton editTestRadioButton;
     private JButton okButton;
     private JButton backButton; //Doesn't do anything yet. Plan to have it return the user to the GUI that called this GUI.
+    private JScrollPane testScrollPane;
+    private JList itemList;
 
     public ChooseActionOnTest() {
         rootPanel.setPreferredSize(new Dimension(300, 200));
 
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        testScrollPane.setVisible(false);
 
         ButtonGroup radioGroup = new ButtonGroup();
         radioGroup.add(createTestRadioButton);
