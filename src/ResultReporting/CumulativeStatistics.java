28bfff6bba1287d61d8a016b472266bd4fd6a1cb
githubuser476 <rebecca.kennedy2@pcc.edu>
2018-06-13 02:22:12 -0700

    Added one statistic for the CumulativeStatistics GUI.

--- src/ResultReporting/CumulativeStatistics.java
+++ src/ResultReporting/CumulativeStatistics.java
@@ -1,12 +1,13 @@
 package ResultReporting;
 
-import SharedLogic.Item;
 import SharedLogic.TestSession;
 import SharedLogic.Result;
+import SharedLogic.ResultReportingItem;
+import SharedLogic.ResultReportingDatabase;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 
@@ -17,115 +18,81 @@ import java.util.List;
  */
 public class CumulativeStatistics {
     private JPanel cumStatPanel;
+    private JTextArea infoTextArea;
+    private JScrollPane cumStatScrollPane;
     private JFrame frame;
 
-  //  private DefaultTableModel matrixTableModel;
     private String testName;
-    private List<ReportingItem> items;
     private ArrayList<Result> results;
-    private ArrayList<Result> allResults;
-    private Container content;
     private ResultScoresForm rsf;
-    private int testSessionID;
     private int testID;
-    private Data data;
-    private ArrayList<ReportingItem> highestScoredItems;
+    private ResultReportingDatabase data;
+    ArrayList<ResultReportingItem> reportingItems;
+    private ArrayList<ResultReportingItem> highestScoredItems;
 
     public CumulativeStatistics(JFrame frame, ResultScoresForm resScoForm) {
 
-        cumStatPanel.setPreferredSize(new Dimension(500, 400));
+        cumStatPanel.setPreferredSize(new Dimension(450, 300));
         this.frame = frame;
+        infoTextArea.setMargin(new Insets(10, 15, 10, 15));
 
         rsf = resScoForm;
         getData();
-        printData();
+
+        frame.setTitle("Cumulative Statistics - " + testName);
+
+        organizePopRankItemFirst();
+
+        infoTextArea.setEditable(false);
     } //end constructor
 
-    private void printData() {
-        //System.out.println(testID);
+    /**
+     * Calls the methods that will decide and display this statistics.
+     * This statistic figures out what percent of the population ranks
+     * each item first.
+     */
+    private void organizePopRankItemFirst() {
+        findHighestScoredItems();
+        Object[] objects = popRankItemFirst();
+        printPopRankItemFirst(objects);
     }
 
+    /**
+     * Gets the data needed for this class
+     */
     private void getData() {
         //Get TestSession to get other values
         TestSession ts = rsf.getTestSession();
 
-        //Get the Session ID.
-        testSessionID = ts.getMySessionID();
+        //Gets the Test Name
+        if(ts.getMyTest() != null) {
+            testName = ts.getMyTest().getMyName();
+        }
 
-        //Get the Test ID and Test Name
+        //Gets the Test ID
         if (ts.getMyTest() != null) {
             testID = ts.getMyTest().getMyTestID();
-//            testName = ts.getMyTest().getMyName();
         }
-        System.out.println("First testID: " + testID);
 
-        //Get ReportingItems which are a child of SharedLogic.Item and store them in a List
-//        items = rsf.getReportingItems();
+        //Gets all the data from the database
+        data = rsf.getResultReportingDatabase();
+    }
 
-        //Get Results
-        //      results = rsf.getResults();
+    /**
+     * Figures out which of the items got the highest scores and then
+     * stores them in an ArrayList
+     */
+    public void findHighestScoredItems() {
 
-
-      //  allResults = new ArrayList<>();
-        data = new Data();
-
-/*
-        data.getResults().forEach(dataResult -> {
-            //Item1 and Item2 should belong to the same test so I will just get Item1's TestID
-            if (dataResult.getMyItemOne().getMyTestID() == testID) {
-                allResults.add(dataResult);
-            }
-        });
-
-
-        System.out.println("ALL RESULTS");
-        for (Result rs : allResults) {
-            System.out.println();
-            System.out.println("TestID: " + rs.getMyItemOne().getMyTestID());
-            System.out.println("ResultID: " + rs.getMyResultID());
-            System.out.println("Item1: " + rs.getMyItemOne().getMyName());
-            System.out.println("Item2: " + rs.getMyItemTwo().getMyName());
-        }
-        */
-
-/*
-        ArrayList<ReportingItem> reportingItems = ReportingItem.buildReportingItems(allResults);
-
-        System.out.println("REPORTING ITEMS");
-        for (ReportingItem item : reportingItems) {
-            int score = item.getScore();
-            System.out.println();
-            System.out.println("Name: " + item.getMyName());
-            System.out.println("Score: " + score);
-        }
-        */
-
-
-
-/*
-        for (ReportingItem item : items) {
-            int score = item.getScore();
-            System.out.println();
-            System.out.println("Name: " + item.getMyName());
-            System.out.println("Score: " + score);
-        }
-*/
-
-
-        //CODE I'M WORKING ON
         ArrayList<TestSession> allTestSessions = new ArrayList<>();
+        highestScoredItems = new ArrayList<>();
 
-        System.out.println("testID: " + testID);
-
-
-        System.out.println("DATARESULT");
         data.getTestSessions().forEach(dataResult -> {
             if (dataResult.getMyTest().getMyTestID() == testID) {
                 allTestSessions.add(dataResult);
             }
         });
 
-        System.out.println("CODE I'M WORKING ON");
         for (TestSession testSession : allTestSessions) {
 
             results = new ArrayList<>();
@@ -137,178 +104,182 @@ public class CumulativeStatistics {
                         results.add(dataResult);
                     }
                 });
-
-                System.out.println("SessionID:" + id);
-                }//NEW
-
-                ArrayList<ReportingItem> reportingItems = ReportingItem.buildReportingItems(results);
-
-            boolean allEqual = true;
-            for(ReportingItem item : reportingItems) {
-                if(item.getScore() != (reportingItems.get(0).getScore())) {
-                    allEqual = false;
-                }
             }
 
-            highestScoredItems = new ArrayList<>();
-            ReportingItem highest = reportingItems.get(0);
+            reportingItems = ResultReportingItem.buildReportingItems(results);
 
-            if(allEqual == false) {
-
-
-
-                System.out.println("REPORTING ITEMS");
-                for (ReportingItem item : reportingItems) {
-                    // addHighestScoredItems(item);
-
-                    //  highestScoredItems = new ArrayList<>();
-                    //   ReportingItem highest = reportingItems.get(0);
-
-                    System.out.println();
-                    System.out.println("highest.getMyName(): " + highest.getMyName());
-                    System.out.println("highest.getScore(): " + highest.getScore());
-                    System.out.println("item.getMyName(): " + item.getMyName());
-                    System.out.println("item.getScore(): " + item.getScore());
-                    System.out.println();
-
-                    if (item.getScore() > highest.getScore()) {
-                        //maxItemCharLength = charLength;
-                        System.out.println("Before highest: " + highest.getMyName());
-                        System.out.println("Before item: " + item.getMyName());
-                        highest = item;
-                        System.out.println("After highest: " + highest.getMyName());
-                        System.out.println("After item: " + item.getMyName());
-                    }
-                    //ORIG              }
-
-
-                    System.out.println("Right before adding highest: " + highest.getMyName());
-                    System.out.println("Right before adding highest: " + highest.getScore());
-
-                    /*******
-                     *  Note to self:
-                     *  Right now I don't think this supports multiple winners. Maybe that's the problem?
-                     *  For example, in test 5, football, basketball, and badminton are all tied for the
-                     *  top with 1 point each.
-                     ******/
-
-                    highestScoredItems.add(highest);
-                }
-
+            if(areAllEqual(reportingItems) == false) {
+                addHighestItems(reportingItems);
             }
-
-        //    }
         } //end for loop
-
-        System.out.println("HighestScoredItems");
-        for(ReportingItem item : highestScoredItems) {
-            int score = item.getScore();
-            System.out.println();
-            System.out.println("TestID: " + item.getMyTestID());
-            System.out.println("ItemID: " + item.getMyItemID());
-            System.out.println("Name: " + item.getMyName());
-            System.out.println("Score: " + score);
-        }
-
-        /*
-        results = new ArrayList<>();
-        if (testSession != null) {
-            int id = testSession.getMySessionID();
-
-            data.getResults().forEach(dataResult -> {
-                if (dataResult.getMySessionID() == id) {
-                    results.add(dataResult);
-                }
-            });
-
-            ArrayList<ReportingItem> reportingItems = ReportingItem.buildReportingItems(results);
-        }
-*/
-
-
-        /*
-        ArrayList<TestSession> testSessions = data.getTestSessions();
-
-        results = new ArrayList<>();
-        for (TestSession testSession : testSessions) {
-            if (testSession != null) {
-                int id = testSession.getMySessionID();
-
-                data.getResults().forEach(dataResult -> {
-                    if (dataResult.getMySessionID() == id) {
-                        results.add(dataResult);
-                    }
-                });
-            }
-
-                for (ReportingItem item : items) {
-                    int score = item.getScore();
-                    System.out.println();
-                    System.out.println("Name: " + item.getMyName());
-                    System.out.println("Score: " + score);
-                }
-            }
-
-
-
-
-
-            results = new ArrayList<>();
-        if (testSession != null) {
-            int id = testSession.getMySessionID();
-
-            data.getResults().forEach(dataResult -> {
-                if (dataResult.getMySessionID() == id) {
-                    results.add(dataResult);
-                }
-            });
-            */
     }
 
-/*
-    //private ArrayList<ReportingItem> getHighestScoredItems(ArrayList<ReportingItem> reportingItems) {
-     private void addHighestScoredItems(ReportingItem item) {
+    /**
+     * Creates an ArrayList of of ResultReportingItem names as Strings.
+     * @param arrayList
+     * @return names
+     */
+    private ArrayList<String> namesArrayList(ArrayList<ResultReportingItem> arrayList) {
+        ArrayList<String> names = new ArrayList<>();
+        for(ResultReportingItem item : arrayList) {
+            names.add(item.getMyName());
+        }
+        return names;
+    }
 
-        highestScoredItems = new ArrayList<>();
-        ReportingItem highest = reportingItems.get(0);
+    /**
+     * Creates an ArrayList of of String names as Strings.
+     * @param arrayList
+     * @return names
+     */
+    public ArrayList<String> namesStrArrayList(ArrayList<String> arrayList) {
+        ArrayList<String> names = new ArrayList<>();
+        for(String item : arrayList) {
+            names.add(item);
+        }
+        return names;
+    }
 
-    //    System.out.println("REPORTING ITEMS");
-      //  for (ReportingItem item : reportingItems) {
-        //    int score = item.getScore();
-        //    System.out.println();
-        //    System.out.println("TestID: " + item.getMyTestID());
-       //     System.out.println("Name: " + item.getMyName());
-        //    System.out.println("Score: " + score);
-        //    System.out.println("Can you see me here?");
-            System.out.println("item.getScore(): " + item.getScore());
-            System.out.println("highest.getScore(): " + highest.getScore());
+    /**
+     * Decides what percent of the population ranks each item first.
+     * @return objects
+     */
+    public Object[] popRankItemFirst() {
+        ArrayList<String> names = namesArrayList(highestScoredItems);
 
-            if (item.getScore() > highest.getScore()) {
-                //maxItemCharLength = charLength;
-                System.out.println("Before highest: " + highest.getMyName());
-                System.out.println("Before item: " + item.getMyName());
-                highest = item;
-                System.out.println("After highest: " + highest.getMyName());
-                System.out.println("After item: " + item.getMyName());
+        List<String> namesList = names;
+        Set<String> namesSet = new HashSet<>(namesList);
+
+        HashMap<String, Float> map = new HashMap<>();
+
+        infoTextArea.append("Population percentage that ranked an item most preferred:\n");
+
+        for(String name : namesSet) {
+            float decimal = (((float)Collections.frequency(namesList, name)) / (float)highestScoredItems.size());
+            float perc = percent(decimal);
+            map.put(name,perc);
+        }
+
+        Object[] objects = map.entrySet().toArray();
+        sortItemsByPerc(objects);
+
+        return objects;
+    }
+
+    /**
+     * Sorts items and their respective percentage in decending order by their percent value.
+     * @param objects
+     */
+    private void sortItemsByPerc(Object[] objects) {
+        Arrays.sort(objects, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Float>) o2).getValue().compareTo(((Map.Entry<String, Float>) o1).getValue());
             }
-     //   }
+        });
+    }
 
+    /**
+     * Calculates the percent value of the given decimal
+     * @param decimal
+     * @return
+     */
+    private float percent(float decimal) {
+        float percent = decimal * 100;
+        return percent;
+    }
+
+    /**
+     * Creates and returns a string of the item and its respective percent value.
+     * This string is expected to be used in the JTestArea.
+     * @param item
+     * @param percent
+     * @return
+     */
+    private String getItemAndPercStr(String item, Float percent) {
+        String percentStr = String.format("%%%4.1f", percent);
+        String str = item + ": \t" + percentStr + "\n";
+        return str;
+    }
+
+    /**
+     * Prints the popRankItemFirst() data in the JTextArea.
+     * @param objects
+     */
+    private void printPopRankItemFirst(Object[] objects) {
+
+        for (Object obj : objects) {
+            String name = ((Map.Entry<String, Float>) obj).getKey();
+            float percent = ((Map.Entry<String, Float>) obj).getValue();
+            String str = getItemAndPercStr(name, percent);
+            infoTextArea.append(str);
+        }
+
+        ArrayList<String> names = namesArrayList(reportingItems);
+
+        ArrayList<String> objStrs = new ArrayList<>();
+        for (Object obj : objects) {
+            String name = ((Map.Entry<String, Float>) obj).getKey();
+            objStrs.add(name);
+        }
+
+        ArrayList<String> objNames = namesStrArrayList(objStrs);
+
+        for (String itemName : names) {
+
+            boolean anyEqual = false;
+            for (String objName : objNames) {
+                if (objName.equals(itemName)) {
+                        anyEqual = true;
+                }
+            } //end for loop
+
+            if(anyEqual == false) {
+                float zeroPerc = 0;
+                String str = getItemAndPercStr(itemName, zeroPerc);
+                infoTextArea.append(str);
+            } //end if
+
+        } //end for loop
+    }
+
+    /**
+     * Checks if the given items' scores are all equal
+     * @param reportingItems
+     * @return allEqual
+     */
+    public boolean areAllEqual(ArrayList<ResultReportingItem> reportingItems) {
+        boolean allEqual = true;
+        for (ResultReportingItem item : reportingItems) {
+            if (item.getScore() != (reportingItems.get(0).getScore())) {
+                allEqual = false;
+            }
+        }
+        return allEqual;
+    }
+
+    /**
+     * Adds the items with the highest scores to highestScoredItems
+     * @param reportingItems
+     */
+    public void addHighestItems(ArrayList<ResultReportingItem> reportingItems) {
+        ResultReportingItem highest = reportingItems.get(0);
+
+        //Adds the last item in the ArrayList that had the highest score
+        for (ResultReportingItem item : reportingItems) {
+            if (item.getScore() > highest.getScore()) {
+                highest = item;
+            }
+        }
 
         highestScoredItems.add(highest);
-        //return highestScoredItems;
 
-        /*
-        int maxItemCharLength = matrixTable.getColumnModel().getColumn(0).getMinWidth();
-        for (ReportingItem item : items) {
-            int charLength = item.getMyName().length();
-            if (charLength > maxItemCharLength) {
-                maxItemCharLength = charLength;
+        //Adds any other items that had the same score
+        for (ResultReportingItem item : reportingItems) {
+            if((item.getScore() == highest.getScore()) && (item.getMyName().equals(highest.getMyName()) == false)) {
+                highestScoredItems.add(item);
             }
         }
-        return maxItemCharLength;
-        */ /*
     }
-    */
 
     /**
      * Getter for cumStatPanel
